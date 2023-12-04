b1c647bdb83bdce9a3f8be8c52813c04c052fcaa
Hunterpanpan <97939292+Hunterpanpan@users.noreply.github.com>
2023-12-04 23:05:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,32 +8,32 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxe0a4193afc11f552',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '19060eb83a97bfe97aed3f9ba6b23102',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '山东',
+  CITY: '德州',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '成成',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oULdL6zhXs0QpQKIFhDpw-XAY_mc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '	wgQ2OO2bTavUkAxwQSPGj5dp3SAjxWnBmSqcZR4vhRI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '02-07',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '成成', year: '2001', date: '02-07',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '成成', year: '2001', date: '02-07',
         },
         {
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
